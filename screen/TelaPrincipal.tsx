559cd5453622d585f089e9e83276761e42dc7d0a
Sid <sidalbertok@gmail.com>
2026-09-02 22:55:50 -0300

    feat: Adiciona gif ao menu iniciar
    fix: conserta logica da tela de treinar

--- screen/TelaPrincipal.tsx
+++ screen/TelaPrincipal.tsx
@@ -1,28 +1,33 @@
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View,TouchableOpacity,ImageBackground } from 'react-native'
 import React from 'react'
 
 export default function TelaPrincipal({navigation}:any) {
   return (
+    <ImageBackground
+      source={{ uri: 'https://i.pinimg.com/originals/1a/d0/11/1ad011529ccd9c2c515a609d8586bab9.gif' }}
+      style={styles.background}
+    >
     <View style={styles.container}>
       
         <Text style={styles.titulo}>
-          Tela Principal | Escolha a Ação
+          Tela Principal | O que quer fazer?
         </Text>
         <View style={styles.botoes}>
             <TouchableOpacity
             style={styles.botao}
             onPress={() => navigation.navigate('TelaTreinar')}
           >
-            <Text style={styles.textoBotao}>Tela Treinar</Text>
+            <Text style={styles.textoBotao}>Treinar Pokemon</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.botao}
             onPress={() => navigation.navigate('TelaLista')}
           >
-            <Text style={styles.textoBotao}>Tela Lista Pokemons</Text>
+            <Text style={styles.textoBotao}>Ver todos Pokemons</Text>
           </TouchableOpacity>
         </View>
      </View>
+     </ImageBackground>
   )
 }
 
@@ -30,13 +35,15 @@ const styles = StyleSheet.create({
     container: {
     flex: 1,
     padding: 20,
+    display: 'flex',
+    justifyContent: 'center'
   },
 
   titulo: {
     fontSize: 30,
     fontWeight: '900',
     textAlign: 'center',
-    color: 'black',
+    color: 'white',
     marginBottom: 20,
   },
 
@@ -63,4 +70,7 @@ const styles = StyleSheet.create({
     fontWeight: '900',
     textAlign: 'center',
   },
+  background:{
+    flex: 1
+  }
 })
--- screen/TelaTreinar.tsx
+++ screen/TelaTreinar.tsx
@@ -1,154 +1,200 @@
 import React, { useState, useEffect } from 'react'
-import { Avatar, Button, Card, Text } from 'react-native-paper';
-import { StyleSheet,View } from 'react-native'
+import { Button, Card, Text } from 'react-native-paper';
+import { StyleSheet, View, Image, Modal, TouchableOpacity } from 'react-native'
 import axios from 'axios'
+import { useAudioPlayer } from 'expo-audio'
 
-export default function TelaTreinar() {
-  const [alimentar,setAlimentar]=useState(0)
-  const [brincar,setBrincar]=useState(0)
-  const [limpar,setLimpar]=useState(0)
-  const [dormir,setDormir]=useState(100)
-  const [pokemons, setpokemons] = useState([])
-  const [carregando,setcarregando]=useState(true)
-  const [busca,setbusca]=useState('')
-  const [pokemonSorteado, setPokemonSorteado] = useState()
-  const filtrados = pokemons.filter((item: any) =>item.name.includes(busca.toLowerCase())
-)
+const pokemonsIniciais = [
+  { name: "cyndaquil", id: 155 },
+  { name: "pikachu", id: 25 },
+  { name: "squirtle", id: 7 },
+  { name: "bulbasaur", id: 1 }
+]
 
-const getId = (url) => {
-    const partes = url.split('/')
-    const id = partes[partes.length - 2] // Pega o número antes da última barra
-    return id
-  }
-
-    const acaoAlimentar = () => {
-    setAlimentar(alimentar + 10)
-    setDormir(Math.max(0, dormir - 10)) // Reduz sono, mas não deixa abaixo de 0
-    console.log(`Pokemon foi alimentado! 🍖 (Comida: ${alimentar + 10}, Sono: ${dormir - 10})`)
-  }
-
-  // FUNÇÃO BRINCAR - reduz sono em 15
-  const acaoBrincar = () => {
-    setBrincar(brincar + 10)
-    setDormir(Math.max(0, dormir - 15)) // Brincar gasta mais sono
-    console.log(`Pokemon brincou! 🎾 (Brincar: ${brincar + 10}, Sono: ${dormir - 15})`)
-  }
-
-  // FUNÇÃO LIMPAR - reduz sono em 5
-  const acaoLimpar = () => {
-    setLimpar(limpar + 10)
-    setDormir(Math.max(0, dormir - 5))
-    console.log(`Pokemon foi limpo! 🧹 (Limpeza: ${limpar + 10}, Sono: ${dormir - 5})`)
-  }
-
-  // FUNÇÃO DORMIR - aumenta sono em 100
-  const acaoDormir = () => {
-    setDormir(Math.min(100, dormir + 100)) // Não passa de 100
-    console.log(`Pokemon dormiu! 😴 (Sono: ${Math.min(100, dormir + 100)})`)
-  }
-
-const acaoSortear = () => {
-  if (pokemons.length > 0) {
-    const indiceAleatorio = Math.floor(Math.random() * pokemons.length)
-    setPokemonSorteado(pokemons[indiceAleatorio])
-    console.log(`Pokémon sorteado: ${pokemons[indiceAleatorio].name}`)
-  } else {
-    console.log('Não há pokémons para sortear')
-  }
+const evolucoes = {
+  bulbasaur:  { nivel: 16, evolui: "ivysaur",     id: 2   },
+  ivysaur:    { nivel: 32, evolui: "venusaur",    id: 3   },
+  squirtle:   { nivel: 16, evolui: "wartortle",   id: 8   },
+  wartortle:  { nivel: 36, evolui: "blastoise",   id: 9   },
+  pikachu:    { nivel: 20, evolui: "raichu",      id: 26  },
+  cyndaquil:  { nivel: 14, evolui: "quilava",     id: 156 },
+  quilava:    { nivel: 36, evolui: "typhlosion",  id: 157 },
 }
 
-  useEffect(()=>{
-        axios.get('https://pokeapi.co/api/v2/pokemon?limit=5000')
-        .then(resposta=>{
-           setpokemons(resposta.data.results)
-            setcarregando(false)
-             const indice = Math.floor(Math.random() * resposta.data.results.length)
-        setPokemonSorteado(resposta.data.results[indice])
-        })
-        
-},[])
 
-  
+export default function TelaTreinar() {
+  const [alimentar, setAlimentar] = useState(0)
+  const [brincar, setBrincar] = useState(0)
+  const [limpar, setLimpar] = useState(0)
+  const [dormir, setDormir] = useState(100)
+  const [pokemonSorteado, setPokemonSorteado] = useState<any>()
+  const [escolhendo, setEscolhendo] = useState(false)
+  const [level,setLevel] = useState(0)
+
+  const player = useAudioPlayer(require('../pok.mp3'))
+  const player2 = useAudioPlayer(require('../up.mp3'))
+
+
+  const tocar = async () => {
+    await player.seekTo(0)
+    player.play()
+  }
+
+  const tocar2 = async () => {
+    await player2.seekTo(0)
+    player2.play()
+  }
+
+  const getId = (url: any) => {
+    const partes = url.split('/')
+    return partes[partes.length - 2]
+  }
+
+  const acaoAlimentar = () => {
+    setAlimentar(alimentar + 10)
+    setLevel(level + 1)
+    setDormir(Math.max(0, dormir - 10))
+  }
+
+  const acaoBrincar = () => {
+    setBrincar(brincar + 10)
+    setLevel(level + 1)
+    setDormir(Math.max(0, dormir - 15))
+  }
+
+  const acaoLimpar = () => {
+    setLimpar(limpar + 10)
+    setLevel(level + 1)
+    setDormir(Math.max(0, dormir - 5))
+  }
+
+  const acaoDormir = () => {
+    setDormir(Math.min(100, dormir + 100))
+  }
+
+  const escolherPokemon = (poke: any) => {
+    setPokemonSorteado({ name: poke.name, url: `https://pokeapi.co/api/v2/pokemon/${poke.id}/` })
+    setEscolhendo(false)
+  }
+
+  useEffect(() => {
+  if (!pokemonSorteado) return
+
+  const nomeAtual = pokemonSorteado.name.toLowerCase() as keyof typeof evolucoes
+
+  const evolucao = evolucoes[nomeAtual]
+
+  if (evolucao && level >= evolucao.nivel) {
+     tocar2()
+    setPokemonSorteado({
+      name: evolucao.evolui,
+      url: `https://pokeapi.co/api/v2/pokemon/${evolucao.id}/`
+    })
+    setLevel(0)
+  }
+}, [level])
+
   return (
-      <View style={styles.tela}>
-    <View style={styles.topo}>
-      <Text style={styles.titulo}>Minha Pokédex</Text>
-    </View>
-    <View style={styles.container}>
-     <Card style={styles.card}>
-    <Card.Actions>
-    <Button onPress={acaoSortear}>🎲 Sortear Pokémon</Button>
-    </Card.Actions>
-    <Card.Cover 
-          style={styles.img} 
-          source={{ 
-            uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonSorteado ? getId(pokemonSorteado.url) : '1'}.png` 
-          }} 
-        />
-    <Card.Actions>
-      
-      <Button onPress={acaoAlimentar}>Alimentar</Button>
-        <Button style={styles.botao} onPress={acaoBrincar}>Brincar</Button>
-        <Button onPress={acaoLimpar}>Limpar</Button>
-        <Button onPress={acaoDormir}>Dormir</Button>
-    </Card.Actions>
-     <Text style={styles.texto}>Pokemon Sorteado: {pokemonSorteado ? pokemonSorteado.name.toUpperCase() : 'Nenhum'}</Text>
-    <Text style={styles.texto}>Fome:{alimentar}</Text>
-    <Text style={styles.texto}>Energia:{brincar}</Text>
-    <Text style={styles.texto}>Sujeira:{limpar}</Text>
-    <Text style={styles.texto}>Sono:{dormir}</Text>
-  </Card>
-    </View>
+    <View style={styles.tela}>
+      <View style={styles.container}>
+        <Card style={styles.card}>
+          <View style={styles.topo}>
+            <Text style={styles.titulo}>Minha Pokédex</Text>
+            <Image
+              source={{ uri: 'https://img.icons8.com/color/80/ultra-ball.png' }}
+              style={styles.logo}
+            />
+          </View>
+            <Text style={styles.level}>Nivel Atual:{level}</Text>
+          <Card.Actions>
+            <Button style={styles.botao} onPress={() => setEscolhendo(true)}>
+              🎮 Escolher Inicial
+            </Button>
+          </Card.Actions>
+
+          <Card.Cover
+            style={styles.img}
+            source={{
+              uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonSorteado ? getId(pokemonSorteado.url) : '1'}.png`
+            }}
+          />
+
+          <Card.Actions>
+            <Button style={styles.botao2} onPress={acaoAlimentar}>Alimentar</Button>
+            <Button style={styles.botao} onPress={acaoBrincar}>Brincar</Button>
+            <Button style={styles.botao3} onPress={acaoLimpar}>Limpar</Button>
+            <Button style={styles.botao4} onPress={acaoDormir}>Dormir</Button>
+          </Card.Actions>
+
+          <Text style={styles.texto}>
+            Pokemon: {pokemonSorteado ? pokemonSorteado.name.toUpperCase() : 'Nenhum'}
+          </Text>
+          <Text style={styles.texto}>Fome: {alimentar}</Text>
+          <Text style={styles.texto}>Energia: {brincar}</Text>
+          <Text style={styles.texto}>Sujeira: {limpar}</Text>
+          <Text style={styles.texto}>Sono: {dormir}</Text>
+        </Card>
+      </View>
+
+      <Modal
+        visible={escolhendo}
+        transparent={true}
+        animationType="fade"
+        onRequestClose={() => setEscolhendo(false)}
+      >
+        <View style={styles.modalFundo}>
+          <View style={styles.modalBox}>
+            <Text style={styles.modalTitulo}>Escolha seu Pokémon!</Text>
+
+            <View style={styles.modalGrid}>
+              {pokemonsIniciais.map((poke) => (
+                <TouchableOpacity
+                  key={poke.name}
+                  style={styles.modalCard}
+                  onPress={()=>{
+                    tocar()
+                   escolherPokemon(poke)
+                  }
+                  }
+                >
+                  <Image
+                    source={{
+                      uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png`
+                    }}
+                    style={styles.modalImg}
+                  />
+                  <Text style={styles.modalNome}>{poke.name.toUpperCase()}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+
+            <Button onPress={() => setEscolhendo(false)}>Cancelar</Button>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  tela: {
-    flex: 1,
-    backgroundColor: '#F5F5F5',
-  },
-
-  topo: {
-    height: 80,
-    backgroundColor: '#DC0A2D',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 20,
-  },
-
-  titulo: {
-    fontSize: 26,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 5,
-    backgroundColor: '#DC0A2D',
-  },
-
-  texto: {
-    fontSize: 20,
-    color: 'white',
-    fontWeight: '900',
-  },
-
-  card: {
-    backgroundColor: '#3B82F6',
-    elevation: 5,
-    borderRadius: 5,
-  },
-
-  img: {
-    backgroundColor: '#1E40AF',
-    borderRadius: 50,
-  },
-
-  botao: {
-    backgroundColor: 'black',
-  },
+  tela: { flex: 1, backgroundColor: '#F5F5F5' },
+  topo: { height: 150, backgroundColor: '#000000', justifyContent: 'center', alignItems: 'center' },
+  titulo: { fontSize: 26, fontWeight: 'bold', color: 'white', textAlign: 'center' },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', padding: 5, backgroundColor: '#DC0A2D' },
+  texto: { fontSize: 20, color: 'white', fontWeight: '900', textAlign: 'center' },
+  card: { backgroundColor: '#f50000ce', elevation: 5, borderRadius: 10, borderColor: 'black', borderWidth: 6 },
+  img: { backgroundColor: '#ffffff', borderRadius: 10, borderColor: 'black', borderWidth: 3 , height: 300},
+  botao: { backgroundColor: 'black', marginRight: 1 },
+  botao2: { backgroundColor: 'red' },
+  botao3: { backgroundColor: 'blue' },
+  botao4: { backgroundColor: 'green' },
+  logo: { width: 100, height: 100, borderColor: 'white', borderWidth: 2, borderRadius: 80 },
+  modalFundo: { flex: 1, backgroundColor: 'rgba(0,0,0,0.7)', justifyContent: 'center', alignItems: 'center' },
+  modalBox: { backgroundColor: '#3B82F6', borderRadius: 15, padding: 20, borderColor: 'black', borderWidth: 4, width: '85%' },
+  modalTitulo: { fontSize: 22, fontWeight: 'bold', color: 'white', textAlign: 'center', marginBottom: 15 },
+  modalGrid: { flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', gap: 10 },
+  modalCard: { backgroundColor: '#1E40AF', borderRadius: 10, padding: 10, alignItems: 'center', width: '45%', borderColor: 'black', borderWidth: 2 },
+  modalImg: { width: 80, height: 80 },
+  modalNome: { color: 'white', fontWeight: 'bold', marginTop: 5 },
+  level: {fontSize:35,color: 'white',fontWeight:900}
 })
